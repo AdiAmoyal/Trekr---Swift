4baa3a3308a53d57d59e0ddeed05be87fe48ae0d
adiamoyal <adi.amoyal02@gmail.com>
2023-02-07 16:09:53 +0200

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Adi Amoyal on 07/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
